bc1492e7a0cd008be1d43de5e17efd1edcecb560
jesuscirineohytech <jesus.cirineo@hytechgizmo.com>
2024-11-14 02:37:04 -0800

    chore: adding datasources and repositories

--- lib/features/tasks/data/datasources/tasks_datasource_impl.dart
+++ lib/features/tasks/data/datasources/tasks_datasource_impl.dart
@@ -1,4 +1,30 @@
 
-TasksDatasourceImpl implements TasksDatasource{
 
+
+import 'package:task_app/core/utils/resources/data_state.dart';
+
+import 'package:task_app/features/tasks/data/DTOs/task_dto.dart';
+
+import 'package:task_app/features/tasks/domain/entities/task.dart';
+
+import '../../domain/datasources/tasks_datasource.dart';
+
+class TasksDatasourceImpl implements TasksDatasource {
+  @override
+  Future<DataState> createTask(TaskDto task) {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteTaskById(String id) {
+    // TODO: implement deleteTaskById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<DataState<Task>> getTask() {
+    // TODO: implement getTask
+    throw UnimplementedError();
+  }
 }
